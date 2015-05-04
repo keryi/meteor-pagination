320dbd29b61124a5632a91a5d8c13aae9a3951f4
keryi <hti_32@yahoo.com>
2015-05-04 10:43:52 +0800

    Moved shared methods to prototype

--- pagination.js
+++ pagination.js
@@ -2,39 +2,52 @@ Pagination = function(Coll, options) {
   this.Coll = Coll;
   this.perPage = options.perPage;
   this.currentIndex = new ReactiveVar(0);
-  this.itemCount = function() {
-    return this.Coll.find().count();
-  };
-  this.pageCount = function() {
-    return Math.ceil(this.itemCount() / this.perPage);
-  };
-  this.currentPage = function() {
-    var startIndex = this.currentIndex.get() * this.perPage;
-    var endIndex = this.hasNext() ?
-      this.itemCount() - 1 : startIndex + this.perPage;
-    return this.Coll.find({}, {limit: (this.currentIndex.get() + 1) * this.perPage})
-      .fetch().slice(startIndex, endIndex);
-  };
-  this.hasNext = function() {
-    return this.currentIndex.get() < this.pageCount() - 1;
-  };
-  this.hasPrevious = function() {
-    return this.currentIndex.get() > 0;
-  };
-  this.goNext = function() {
-    if (this.hasNext()) {
-      this.currentIndex.set(this.currentIndex.get() + 1);
-    }
-  };
-  this.goPrevious = function() {
-    if (this.hasPrevious()) {
-      this.currentIndex.set(this.currentIndex.get() - 1);
-    }
-  };
-  this.goFirst = function() {
-    this.currentIndex.set(0);
-  };
-  this.goLast = function() {
-    this.currentIndex.set(this.pageCount() - 1);
-  };
+};
+
+Pagination.prototype.itemCount = function() {
+  return this.Coll.find().count();
+};
+
+Pagination.prototype.itemCount = function() {
+  return this.Coll.find().count();
+};
+
+Pagination.prototype.pageCount = function() {
+  return Math.ceil(this.itemCount() / this.perPage);
+};
+
+Pagination.prototype.currentPage = function() {
+  var startIndex = this.currentIndex.get() * this.perPage;
+  var endIndex = this.hasNext() ?
+    this.itemCount() - 1 : startIndex + this.perPage;
+  return this.Coll.find({}, {limit: (this.currentIndex.get() + 1) * this.perPage})
+    .fetch().slice(startIndex, endIndex);
+};
+
+Pagination.prototype.hasNext = function() {
+  return this.currentIndex.get() < this.pageCount() - 1;
+};
+
+Pagination.prototype.hasPrevious = function() {
+  return this.currentIndex.get() > 0;
+};
+
+Pagination.prototype.goNext = function() {
+  if (this.hasNext()) {
+    this.currentIndex.set(this.currentIndex.get() + 1);
+  }
+};
+
+Pagination.prototype.goPrevious = function() {
+  if (this.hasPrevious()) {
+    this.currentIndex.set(this.currentIndex.get() - 1);
+  }
+};
+
+Pagination.prototype.goFirst = function() {
+  this.currentIndex.set(0);
+};
+
+Pagination.prototype.goLast = function() {
+  this.currentIndex.set(this.pageCount() - 1);
 };
